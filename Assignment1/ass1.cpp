c15ffb85da5d20edf1500c58fda47ee5164fd892
zwan074 <42155686+zwan074@users.noreply.github.com>
2020-08-06 10:47:50 +1200

    Add files via upload

--- Assignment1/ass1.cpp
+++ Assignment1/ass1.cpp
@@ -7,7 +7,7 @@
 #define m 4294967296
 #define c 1013904223
 #define n0 12345
-#define N 10000//10000000
+#define N 1000//10000000
 #define sidelen 65536
 
 typedef unsigned long ULONG;
@@ -97,8 +97,8 @@ int main(int argc,char* argv[])
             MPI_Recv(&number_in_circle1, 1, MPI_UNSIGNED_LONG, i,0, MPI_COMM_WORLD, &Stat);   
             //MPI::COMM_WORLD.Recv(&number_in_circle1, 1, MPI::UNSIGNED_LONG, i, 0);
             number_in_circle0 += number_in_circle1;
-               
         }
+
         double result = number_in_circle0 / N ;
         std::cout << "result " << result << std::endl;
         fprintf(stdout,"The final result is %d \n",result);
